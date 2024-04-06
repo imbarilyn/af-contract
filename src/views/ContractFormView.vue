df533dd2007ee7fadc3a481882e3b4482d333fec
muhonja <linahmuhonjaimbari>
2024-04-06 22:27:06 +0300

    add validation for vendor name

--- src/views/ContractFormView.vue
+++ src/views/ContractFormView.vue
@@ -1,6 +1,38 @@
 <script>
+import { useField } from 'vee-validate';
+import { reactive, ref, watch} from "vue"
 
+const contractDetails = reactive({
+    vendorName:"",
+    country:"",
+    start_date: "",
+    end_dtate: ""
+})
 
+const vendorNameValidator = (value)=>{
+    if(!value){
+        return "Vendor Name is required"
+    }
+    if(value.lenngth < 3){
+        return "vendor name should be at least 3 characters"
+    }
+    if(value.length > 10){
+        return "vendor name is too long"
+    }
+    else {
+        return true
+    }
+}
+
+const {
+    value:vendorName,
+    errorMessage:vendorNameError,
+    meta : metaVendorName
+} = useField("vendorName", vendorNameValidator)
+
+watch(()=> contractDetails.vendorName, (newVendorName)=>{
+    contractDetails.vendorName = newVendorName
+})
 
 </script>
 
@@ -8,7 +40,7 @@
 
 <template>
     <div class="container">
-        <form>
+        <form @submit="handleSubmit">
             <div class="form-row">
                 <div class="col col-12">
                     <input type="text" class="form-control" placeholder="Vendor name">
@@ -23,6 +55,8 @@
                     <input type="text" class="form-control" placeholder="End date">
                 </div>
             </div>
+            <button class="btn btn-primary" type="submit">Submit</button>
         </form> 
+        
     </div>       
 </template>
