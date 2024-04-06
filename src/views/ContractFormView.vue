6c3570743556612a41d519d4d0292759018b2303
muhonja <linahmuhonjaimbari>
2024-04-06 21:39:24 +0300

    initialize vue app

---
+++ src/views/ContractFormView.vue
@@ -0,0 +1,28 @@
+<script>
+
+
+
+</script>
+
+
+
+<template>
+    <div class="container">
+        <form>
+            <div class="form-row">
+                <div class="col col-12">
+                    <input type="text" class="form-control" placeholder="Vendor name">
+                </div>
+                <div class="col col-12 mb-2rem">
+                    <input type="text" class="form-control" placeholder="Counrty">
+                </div>
+                <div class="col col-12">
+                    <input type="text" class="form-control" placeholder="Start date">
+                </div>
+                <div class="col col-12">
+                    <input type="text" class="form-control" placeholder="End date">
+                </div>
+            </div>
+        </form> 
+    </div>       
+</template>
